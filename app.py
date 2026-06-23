e6bda9be8f03fe4ef59847f792d8af47473c5839
Eric. MJ <ericmanoj1@gmail.com>
2026-06-23 22:34:29 +0530

    Initial version of Status Quo

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+from investigator import check_url, check_website
+
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    
+    result = None
+    if request.method == 'POST':
+        url = request.form.get('url')
+        valid_url = check_url(url)
+        if valid_url:
+            result = check_website(valid_url)
+        else:
+            result = {"Error": "Invalid URL"}
+
+    return render_template('index.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
